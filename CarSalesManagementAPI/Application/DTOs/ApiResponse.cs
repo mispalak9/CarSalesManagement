67959954207e2ce22fab028b963c962120a05b7d
Palak Raj Mishra <71177106+mispalak9@users.noreply.github.com>
2026-01-19 10:55:58 +0530

    Delete CarSalesManagementAPI directory

--- CarSalesManagementAPI/Application/DTOs/ApiResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace CarSalesManagementAPI.Application.DTOs;
-
-public class ApiResponse<T>
-{
-    public bool Success { get; set; }
-    public string Message { get; set; } = string.Empty;
-    public T? Data { get; set; }
-    public List<string> Errors { get; set; } = new();
-}
--- CarSalesManagementAPI/Application/Services/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-using CarSalesManagementAPI.Application.DTOs;
-
-namespace CarSalesManagementAPI.Application.Services;
-
-public interface IAuthService
-{
-    Task<ApiResponse<LoginResponseDto>> Login(LoginDto loginDto);
-    Task<ApiResponse<LoginResponseDto>> GetUserInfo(int userId);
-}
